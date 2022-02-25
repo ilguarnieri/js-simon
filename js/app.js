33dd85d715b1baf29447607bf790dee2f2f8b6f4
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-02-25 22:54:30 +0100

    create html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function getRandom(min,max){
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+const numbers = [];
+let i = 0;
+
+while(i < 5){
+    const numero = getRandom(1,99);
+    
+    
+}
+
+console.log('ciao')
