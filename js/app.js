52bd689897b6e090eeefd1cee3d6498f6867ae4e
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-02-26 21:56:17 +0100

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,53 +1,38 @@
+//timer memory
+const timeWait = 15;
+//timer attesa
+const timeInput = 30;
+
+
 const div_title = document.querySelector('.div_title');
 const time_wrapper = document.querySelector('.time-wrapper');
 const game_start = document.querySelector('.game_start');
-const game_input = document.querySelector('.game_input');
-
 const mind_wait = document.querySelector('.mind_wait');
-
-
-//15
-const timeWait = 1;
-//30
-const timeInput = 2;
-
-
-
 
 
 //btn start
 const cta_start = document.querySelector('.cta_start');
 cta_start.addEventListener('click', start);
 
-//btn restart
-const cta_restart = document.querySelector('.cta_restart');
-cta_restart.addEventListener('click', restart);
 
 //btn send
 const cta_send = document.querySelector('.cta_send');
 
 
-
-
-
-
 //funzione restart
 function restart(){
-    window.location.reload()
+    window.location.reload();
 }
-
-
-
-
-
-
-
-
 
 
 //funzione start game
 function start(){
 
+    //btn restart
+    const cta_restart = document.querySelector('.cta_restart');
+    cta_restart.addEventListener('click', restart);
+
+    //cambio titolo div
     div_title.innerHTML = `Memorizza i seguenti numeri!`;
     time_wrapper.classList.remove('d-none');
 
@@ -56,17 +41,16 @@ function start(){
     cta_restart.classList.remove('d-none');
 
     const numbers = [];
-    let i = 0;
 
     //generazione numeri
-    while(i < 5){
+    while(numbers.length < 5){
         const numero = getRandom(1,99);
         
         if(!numbers.includes(numero)){
-            i++;
             numbers.push(numero);
         }
     }
+
     
     //stampa numeri
     game_start.innerHTML = numbers.join(' - ');
@@ -81,27 +65,22 @@ function start(){
     //listener invio dati
     cta_send.addEventListener('click', sendInput);
 
-    //controllo inserimento dati
-    //confronto array
-
     //funzione invio input utente
     function sendInput(){
 
-        //listener btn
         const numberInput = document.getElementsByClassName('input_number');
-
-        let j = 0;
+        let score = 0;
         const numbersUser = [];
 
-        while(j < 5){
-            const inputValue = numberInput[j];
-            if(inputValue.value !== ''){
+        for(let i = 0; i < 5; i++){
+
+            const inputValue = numberInput[i].value;
+            if(inputValue !== '' && !numbersUser.includes(inputValue)){
                 //input valido
-                j++;
-                numbersUser.push(inputValue.value);
+                numbersUser.push(inputValue);
             }else{
                 //input vuoto
-                alert('ATTENZIONE!\nUna o più caselle sono vuote.');
+                alert('ATTENZIONE!\nUna o più caselle sono vuote o hai inserito lo stesso numero!');
                 return;
             }
         }
@@ -111,21 +90,28 @@ function start(){
 
         for(let k = 0; k < numbersUser.length; k++){
 
-            
+            //cella input
+            const cellInput = numberInput[k];
+            //valori array numeri utente
+            const userVerification = parseInt(numbersUser[k]);
 
+            if(numbers.includes(userVerification)){
+                //numero esatto
+                score++;
+                cellInput.classList.add('green');
+            }else{
+                //numero sbagliato
+                cellInput.classList.add('red');
+            }
+        }
 
+        if(score == 5){
+            div_title.innerHTML = `Wow, sei un genio!`;
+        }else{
+            div_title.innerHTML = `Hai indovinato ${score} numeri su 5!`;
         }
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 //funzione numeri random
@@ -159,11 +145,12 @@ function timeClock(time){
 function timeRemember(){
 
     //array nascosto
+    game_start.innerHTML = '';
     game_start.classList.add('d-none');
     //comparsa mind
     mind_wait.classList.remove('d-none');
     //cancellazione title
-    div_title.innerHTML = ``;
+    div_title.innerHTML = `Ooooom...concetrati`;
     //avvio timer 30 sec
     timeClock(timeInput);
 }
@@ -171,8 +158,15 @@ function timeRemember(){
 
 //funzione comparsa input
 function inputUser(){        
+
+    const game_input = document.querySelector('.game_input');
+    //scomparsa timer e img attesa
     time_wrapper.classList.add('d-none');
     mind_wait.classList.add('d-none');
+    //comparsa input utente
     game_input.classList.remove('d-none');
     cta_send.classList.remove('d-none');
+
+    //titolo
+    div_title.innerHTML = `Scrivi i numeri che ricordi <i class="far fa-meh-rolling-eyes"></i>`;
 }
